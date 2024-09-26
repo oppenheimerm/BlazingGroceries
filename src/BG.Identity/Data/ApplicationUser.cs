425870f094977845d1764171156eb036b6c1ff29
Michiel Oppenheimer <michieloppenheimer@outlook.com>
2024-09-26 15:43:21 +0100

    first commit

---
+++ src/BG.Identity/Data/ApplicationUser.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace BG.Identity.Data;
+
+// Add profile data for application users by adding properties to the ApplicationUser class
+public class ApplicationUser : IdentityUser
+{
+}
+
