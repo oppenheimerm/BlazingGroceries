900a72658844c72554ff46aaf297769083b5348f
michieloppenheimer <michieloppenheimer@gmail.com>
2025-10-09 13:26:59 +0100

    TailwindCss added; bootstrap removed

---
+++ BG/tailwind.config.js
@@ -0,0 +1,16 @@
+﻿/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['./**/*.{razor, html}'],
+    theme: {
+        extend: {
+            colors: {
+                'qp-background': '#3f3d47',
+            },
+        },
+        fontFamily: {
+            inter: ["Inter", "sans-serif"]
+        }
+    },
+    plugins: [],
+}
+
